75d603176ce4b2c43ea9506afd2db3113662bf64
Karla Susana Mendoza Romero <A01566555@itesm.mx>
2019-05-16 12:47:16 -0600

    Initial Commit

---
+++ Problema3/Problema3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Problema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Por favor, escribe tu nombre: ");
+            string name = Convert.ToString(Console.ReadLine());
+            Apellido();
+
+            Console.WriteLine("Presiona enter para continuar...");
+            Console.ReadKey();
+        }
+
+
+        public static void Apellido()
+        {
+            try
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Ahora, escribe tu apellido: ");
+                string Apellido = Convert.ToString(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Formato de apelldo incorrecto, por favor escriba un nombre con mas de dos letras.");
+                Apellido();
+            }
+        }
+    }
+}
+
